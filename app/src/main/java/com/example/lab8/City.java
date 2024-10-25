39610a7c20c82e231eb1e5750b6c407d89d9dc61
Shammir Jose <shammir@ualberta.ca>
2024-10-25 10:01:15 -0600

    implemented equals and hashcode

--- app/src/main/java/com/example/lab8/City.java
+++ app/src/main/java/com/example/lab8/City.java
@@ -17,4 +17,27 @@ public class City {
     String getProvinceName(){
         return this.province;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        // Check if the object is compared with itself
+        if (this == obj) {
+            return true;
+        }
+        // Check if the object is null or not the same class type
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        // Typecast obj to City and compare fields
+        City city = (City) obj;
+        return this.city.equals(city.city) && this.province.equals(city.province);
+    }
+
+    // Override hashCode() method
+    @Override
+    public int hashCode() {
+        int result = city.hashCode();
+        result = 31 * result + province.hashCode();
+        return result;
+    }
 }
